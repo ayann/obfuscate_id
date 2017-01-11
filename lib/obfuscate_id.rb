ebb0cdc23256ae81e62411ea1d9b1e145956c64f
Ricardo Garcia <ricardo@ukko.mx>
2017-01-10 23:40:57 -0600

    Remove missing references

--- lib/obfuscate_id.rb
+++ lib/obfuscate_id.rb
@@ -8,7 +8,7 @@ module ObfuscateId
     include InstanceMethods
   end
 
-  def self.hide(str, spin = nil)
+  def self.hide(str)
     Base64.encode64(str.to_s).gsub(/[\s=]+/, "").tr('+/','-_')
   end
 
@@ -36,25 +36,13 @@ module ObfuscateId
     end
 
     def deobfuscate_id(obfuscated_id)
-      ObfuscateId.show(obfuscated_id)
-    end
-
-    # Generate a default spin from the Model name
-    # This makes it easy to drop obfuscate_id onto any model
-    # and produce different obfuscated ids for different models
-    def obfuscate_id_default_spin
-      alphabet = Array("a".."z")
-      number = name.split("").collect do |char|
-        alphabet.index(char)
-      end
-
-      number.shift(12).join.to_i
+      ObfuscateId.show obfuscated_id
     end
   end
 
   module InstanceMethods
     def to_param
-      ObfuscateId.hide(self.id, self.class.obfuscate_id_spin)
+      ObfuscateId.hide self.id
     end
 
     # Override ActiveRecord::Persistence#reload
